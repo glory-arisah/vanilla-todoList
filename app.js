96e65a021fcc4937c03303a8a9417e86c093cbcf
Glory Arisah <gloryarisah@gmail.com>
2022-04-11 01:33:09 +0100

    added local storage functionality to the application which enables CRUD

--- app.js
+++ app.js
@@ -9,7 +9,52 @@ let editFlag = false
 let editId = ''
 let editElement
 
+// LOCALSTORAGE FUNCTIONS
+const getTodos = () => {
+  return localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : []
+}
+const addToLocalStorage = (id, value) => {
+  let todoParams = {id, value}
+  // fetch items in localStorage todoList property
+  let todoList = getTodos()
+  // add todoParams object to the todoList array
+  todoList.push(todoParams)
+  // set the todoList localStorage prop to the updated todoList array
+  localStorage.setItem('todoList', JSON.stringify(todoList))
+}
+
+const updateLocalStorage = (id, value) => {
+  let todoList = getTodos()
+
+  todoList = todoList.map(todo => {
+    if (todo.id === id) {
+      todo.value = value
+    }
+    return todo
+  })
+  localStorage.setItem('todoList', JSON.stringify(todoList))
+}
+
+const removeFromLocalStorage = id => {
+  let todoList = getTodos()
+
+  todoList = todoList.filter((todo) => {
+    if (todo.id !== id) {
+      return todo
+    }
+  })
+
+  localStorage.setItem('todoList', JSON.stringify(todoList))
+  console.log(localStorage.getItem('todoList'))
+}
+
 // FUNCTIONS
+const defaultSetting = () => {
+  clearAllBtn.classList.remove('clear-all')
+  noTaskAlert.textContent = 'you have no tasks'
+  noTaskAlert.style.display = 'block'
+}
+
 const clearAll = () => {
   let allTodos = document.querySelectorAll('.todo')
   if (allTodos.length > 0) {
@@ -17,8 +62,8 @@ const clearAll = () => {
       todoContainer.removeChild(todo)
     })
   }
-  clearAllBtn.classList.remove('clear-all')
-  noTaskAlert.style.display = 'block'
+  defaultSetting()
+  localStorage.clear()
 }
 
 const editTodo = (e) => {
@@ -27,29 +72,33 @@ const editTodo = (e) => {
   editElement = e.target.parentElement.previousElementSibling.lastElementChild
   userInput.value = editElement.innerHTML
   editFlag = true
-  submitBtn.textContent = 'Update'
+  submitBtn.textContent = 'Update' 
 }
 
 const deleteTodo = (e) => {
   let todoArticle = e.target.parentElement.parentElement
+  let id = todoArticle.dataset.id
   todoContainer.removeChild(todoArticle)
+  console.log(id)
   if (todoContainer.children.length > 1) {
     clearAllBtn.classList.add('clear-all') 
   } else if (todoContainer.children.length <= 1) {
-    clearAllBtn.classList.remove('clear-all')
-    noTaskAlert.style.display = 'block'
+    defaultSetting()
   }
+  removeFromLocalStorage(id)
 }
 
 const createTodo = () => {
   let value = userInput.value
-  let id = new Date().getTime()
+  let id = new Date().getTime().toString()
 
   if (editFlag && value) {
     editElement.textContent = value
-    submitBtn.textContent = 'Submit'
+    updateLocalStorage(editId, value)
     userInput.value = ''
+    submitBtn.textContent = 'Submit'
     editFlag = false
+    editId = ''
   }
   else if (!editFlag && value) {
   const article = document.createElement('article')
@@ -73,24 +122,12 @@ const createTodo = () => {
 
   deleteBtn.addEventListener('click', deleteTodo)
   editBtn.addEventListener('click', editTodo)
+  addToLocalStorage(id, value)
   userInput.value = ''
   }
   else if (!editFlag && !value) {
     noTaskAlert.textContent = 'Please enter a value'
   }
-  
-}
-
-// LOCALSTORAGE FUNCTIONS
-const addToLocalStorage = () => {
-  
-}
-
-const updateLocalStorage = () => {
-  
-}
-
-const removeFromLocalStorage = () => {
   
 }
 
